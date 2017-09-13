ace6db350bec32294f39096e0c9ccf87f2adf187
Dimosthenis Kontogiorgos <diko@kth.se>
2017-09-13 17:21:04 +0200

    Updates dict with all data per frame.

--- src/main/python/processors/mocap_gaze/mocap_gaze.py
+++ src/main/python/processors/mocap_gaze/mocap_gaze.py
@@ -11,6 +11,7 @@ sys.path.append('../..')
 from shared import MessageQueue
 import yaml
 from collections import defaultdict
+import math
 
 DEBUG = True
 
@@ -20,23 +21,42 @@ settings = yaml.safe_load(open(SETTINGS_FILE, 'r').read())
 
 # Dictionaries
 tobiimocap_dict = defaultdict(lambda : defaultdict(dict))
-tobiimocap_dict[0]['0'] = '0'
+
+# Each key is the local timestamp in seconds. The second key is the frame
+tobiimocap_dict[0][0]['device'] = 'body'
 
 # Procees tobii input data
 def tobiicallback(_mq, get_shifted_time, routing_key, body):
-    if DEBUG:
-        print("-------------------------------------------------Tobii-------------------------------------------------")
-        tobiitime = body['localtime']
-        tobiimocap_dict[tobiitime]['tobii'] = body
-        print(tobiimocap_dict[tobiitime])
+    if DEBUG: print("--------------------------------------------------------------------------------------------------")
+
+    # Get tobii localtime
+    tobiitime = body['localtime']
+
+    # First get which second
+    second = int(tobiitime)
+
+    # Get decimals to decide which frame
+    frame = int(math.modf(tobiitime)[0] * 50)
+
+    # Put in dictionary
+    tobiimocap_dict[second][frame][body['name']] = body
+
+    # Print 1 frame before
+    if DEBUG: print(tobiimocap_dict[second][frame-1])
 
 # Procees mocap input data
 def mocapcallback(_mq, get_shifted_time, routing_key, body):
-    if DEBUG:
-        print("-------------------------------------------------Mocap-------------------------------------------------")
-        mocaptime = body['localtime']
-        tobiimocap_dict[mocaptime]['mocap'] = body
-        print(tobiimocap_dict[mocaptime])
+    # Get mocap localtime
+    mocaptime = body['localtime']
+
+    # First get which second
+    second = int(mocaptime)
+
+    # Get decimals to decide which frame
+    frame = int(math.modf(mocaptime)[0] * 50)
+
+    # Put in dictionary
+    tobiimocap_dict[second][frame]['mocap_' + body['name']] = body
 
 mq = MessageQueue('mocaptobii_processing')
 
