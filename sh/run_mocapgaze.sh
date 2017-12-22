94da013d7175c605eba04d04cd98ae7b1cc2c81f
Dimosthenis Kontogiorgos <diko@kth.se>
2017-12-22 21:34:07 +0100

    Updates for running without vis.

--- sh/run_mocapgaze.sh
+++ sh/run_mocapgaze.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /Users/diko/Dropbox/University/PhD/Code/MultiSensoryProcessing/multisensoryprocessing/python/processors/mocap_gaze
-#python mocap_gaze.py 2 13
-websocketd --port=8080 python mocap_gaze.py 1 13
+python mocap_gaze.py 2 13
+#websocketd --port=8080 python mocap_gaze.py 1 13
