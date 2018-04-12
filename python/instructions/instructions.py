8e684dbe334d6a41274f33f36cd8459bc214e9ed
pmilmac2 <dimos@dyna067-188.speech.kth.se>
2018-04-12 13:55:34 +0200

    Updates instructions furhat.

--- python/instructions/instructions.py
+++ python/instructions/instructions.py
@@ -48,159 +48,165 @@ client.start()
 # Define dictionary for message
 feature_dict = defaultdict(lambda : defaultdict(dict))
 
+# Uncomment for furhat and indent
 # Connect to Furhat
-with connect_to_iristk(FURHAT_IP) as furhat_client:
-    # Introduce Furhat
-    furhat_client.say(FURHAT_AGENT_NAME, 'You will instruct the builder on how to construct this item.')
-    furhat_client.gaze(FURHAT_AGENT_NAME, {'x':3.00,'y':0.00,'z':2.00}) # At default P1 position
-    #furhat_client.gaze(FURHAT_AGENT_NAME, {'x':-2.00,'y':0.00,'z':2.00}) # At default P2 position
-    # Log Furhat events
-    def event_callback(event):
-        #print(event) # Receives each event the furhat sends out.
-        fd = open('../../logs/furhat_log.csv','a')
-        fd.write(event)
-        fd.write('\n')
-        fd.close()
-    # Listen to events
-    furhat_client.start_listening(event_callback) # register the event callback receiver
+# with connect_to_iristk(FURHAT_IP) as furhat_client:
+#     # Introduce Furhat
+#     furhat_client.say(FURHAT_AGENT_NAME, 'You will instruct the builder on how to construct this item.')
+#     furhat_client.gaze(FURHAT_AGENT_NAME, {'x':3.00,'y':0.00,'z':2.00}) # At default P1 position
+#     #furhat_client.gaze(FURHAT_AGENT_NAME, {'x':-2.00,'y':0.00,'z':2.00}) # At default P2 position
+#     # Log Furhat events
+#     def event_callback(event):
+#         #print(event) # Receives each event the furhat sends out.
+#         fd = open('../../logs/furhat_log.csv','a')
+#         fd.write(event)
+#         fd.write('\n')
+#         fd.close()
+#     # Listen to events
+#     furhat_client.start_listening(event_callback) # register the event callback receiver
+# Uncomment for furhat and indent
 
-    # Define photos for instructions
-    photo0 = 'photos/0.png'
-    photo1 = "photos/1.png"
-    photo2 = "photos/2.png"
-    photo3 = "photos/3.png"
-    photo4 = "photos/4.png"
-    photo5 = "photos/5.png"
-    photo6 = "photos/6.png"
-    photo7 = "photos/7.png"
-    photo8 = "photos/8.png"
-    photo9 = "photos/9.png"
-    master.photos = {}
-    master.photos[0] = ImageTk.PhotoImage(Image.open(photo0))
-    master.photos[1] = ImageTk.PhotoImage(Image.open(photo1))
-    master.photos[2] = ImageTk.PhotoImage(Image.open(photo2))
-    master.photos[3] = ImageTk.PhotoImage(Image.open(photo3))
-    master.photos[4] = ImageTk.PhotoImage(Image.open(photo4))
-    master.photos[5] = ImageTk.PhotoImage(Image.open(photo5))
-    master.photos[6] = ImageTk.PhotoImage(Image.open(photo6))
-    master.photos[7] = ImageTk.PhotoImage(Image.open(photo7))
-    master.photos[8] = ImageTk.PhotoImage(Image.open(photo8))
-    master.photos[9] = ImageTk.PhotoImage(Image.open(photo9))
+# Define photos for instructions
+photo0 = 'photos/0.png'
+photo1 = "photos/1.png"
+photo2 = "photos/2.png"
+photo3 = "photos/3.png"
+photo4 = "photos/4.png"
+photo5 = "photos/5.png"
+photo6 = "photos/6.png"
+photo7 = "photos/7.png"
+photo8 = "photos/8.png"
+photo9 = "photos/9.png"
+master.photos = {}
+master.photos[0] = ImageTk.PhotoImage(Image.open(photo0))
+master.photos[1] = ImageTk.PhotoImage(Image.open(photo1))
+master.photos[2] = ImageTk.PhotoImage(Image.open(photo2))
+master.photos[3] = ImageTk.PhotoImage(Image.open(photo3))
+master.photos[4] = ImageTk.PhotoImage(Image.open(photo4))
+master.photos[5] = ImageTk.PhotoImage(Image.open(photo5))
+master.photos[6] = ImageTk.PhotoImage(Image.open(photo6))
+master.photos[7] = ImageTk.PhotoImage(Image.open(photo7))
+master.photos[8] = ImageTk.PhotoImage(Image.open(photo8))
+master.photos[9] = ImageTk.PhotoImage(Image.open(photo9))
 
-    # Last photo
-    end = 9
+# Last photo
+end = 9
 
-    # Callback when the button is pressed
-    def nextCallback(index, end):
-        # Define time stamp
-        time = datetime.now()
-        time_stamp = "{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}.{:4}".format(\
-                      time.year, time.month, time.day, time.hour, time.minute, time.second,\
-                      str(time.microsecond)[:3])
+# Callback when the button is pressed
+def nextCallback(index, end):
+    # Define time stamp
+    time = datetime.now()
+    time_stamp = "{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}.{:4}".format(\
+                  time.year, time.month, time.day, time.hour, time.minute, time.second,\
+                  str(time.microsecond)[:3])
 
-        # Define feature dict
-        feature_dict[0][0]['TS'] = time_stamp
-        if index == 0:
-            feature_dict[0][0]['S'] = 'start'
-        elif index == end - 1:
-            feature_dict[0][0]['S'] = 'end'
-        else:
-            feature_dict[0][0]['S'] = 'next'
+    # Define feature dict
+    feature_dict[0][0]['TS'] = time_stamp
+    if index == 0:
+        feature_dict[0][0]['S'] = 'start'
+    elif index == end - 1:
+        feature_dict[0][0]['S'] = 'end'
+    else:
+        feature_dict[0][0]['S'] = 'next'
 
-        # Furhat
-        if index == 0:
-            furhat_client.say(FURHAT_AGENT_NAME, 'Okay lets start.')
-            furhat_client.gaze(FURHAT_AGENT_NAME, {'x':-2.00,'y':0.00,'z':2.00}) # At default P2 position
-        elif index == end - 1:
-            furhat_client.say(FURHAT_AGENT_NAME, 'It seems like you finished this task. Well done.')
-            furhat_client.gaze(FURHAT_AGENT_NAME, {'x':-2.00,'y':0.00,'z':2.00}) # At default P2 position
-        else:
-            furhat_client.say(FURHAT_AGENT_NAME, 'Lets get to the next one.')
-            furhat_client.gaze(FURHAT_AGENT_NAME, {'x':3.00,'y':0.00,'z':2.00}) # At default P1 position
+# Uncomment for Furhat
+    # # Furhat
+    # if index == 0:
+    #     furhat_client.say(FURHAT_AGENT_NAME, 'Okay lets start.')
+    #     furhat_client.gaze(FURHAT_AGENT_NAME, {'x':-2.00,'y':0.00,'z':2.00}) # At default P2 position
+    # elif index == end - 1:
+    #     furhat_client.say(FURHAT_AGENT_NAME, 'It seems like you finished this task. Well done.')
+    #     furhat_client.gaze(FURHAT_AGENT_NAME, {'x':-2.00,'y':0.00,'z':2.00}) # At default P2 position
+    # else:
+    #     furhat_client.say(FURHAT_AGENT_NAME, 'Lets get to the next one.')
+    #     furhat_client.gaze(FURHAT_AGENT_NAME, {'x':3.00,'y':0.00,'z':2.00}) # At default P1 position
+# Uncomment for Furhat
 
-        # Print current frame
-        print(feature_dict[0][0])
+    # Print current frame
+    print(feature_dict[0][0])
 
-        # Update index
-        global global_index
-        global_index = index + 1
+    # Update index
+    global global_index
+    global_index = index + 1
 
-        # Update screen
-        vlabel.configure(image = master.photos[global_index])
+    # Update screen
+    vlabel.configure(image = master.photos[global_index])
 
-        # Sending messages to the server
-        my_message = json.dumps(feature_dict[0][0])
-        my_message = "interpreter;data;" + my_message + "$"
-        # Encode the string to utf-8 and write it to the pipe defined above
-        os.write(pipe_out, my_message.encode("utf-8"))
-        sys.stdout.flush()
+    # Sending messages to the server
+    my_message = json.dumps(feature_dict[0][0])
+    my_message = "interpreter;data;" + my_message + "$"
+    # Encode the string to utf-8 and write it to the pipe defined above
+    os.write(pipe_out, my_message.encode("utf-8"))
+    sys.stdout.flush()
 
-        # Send sync signal
-        playsound('beep.mp3')
+    # Send sync signal
+    playsound('beep.mp3')
 
-    def wrongCallback(index):
-        # Define time stamp
-        time = datetime.now()
-        time_stamp = "{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}.{:4}".format(\
-                      time.year, time.month, time.day, time.hour, time.minute, time.second,\
-                      str(time.microsecond)[:3])
+def wrongCallback(index):
+    # Define time stamp
+    time = datetime.now()
+    time_stamp = "{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}.{:4}".format(\
+                  time.year, time.month, time.day, time.hour, time.minute, time.second,\
+                  str(time.microsecond)[:3])
 
-        # Define feature dict
-        feature_dict[0][0]['TS'] = time_stamp
-        feature_dict[0][0]['S'] = str(index) + '_wrong'
+    # Define feature dict
+    feature_dict[0][0]['TS'] = time_stamp
+    feature_dict[0][0]['S'] = str(index) + '_wrong'
 
-        # Furhat
-        furhat_client.say(FURHAT_AGENT_NAME, 'It seems that is not right.')
-        furhat_client.gaze(FURHAT_AGENT_NAME, {'x':3.00,'y':0.00,'z':2.00}) # At default P1 position
+# Uncomment for Furhat
+    # # Furhat
+    # furhat_client.say(FURHAT_AGENT_NAME, 'It seems that is not right.')
+    # furhat_client.gaze(FURHAT_AGENT_NAME, {'x':3.00,'y':0.00,'z':2.00}) # At default P1 position
+# Uncomment for Furhat
 
-        # Print current frame
-        print(feature_dict[0][0])
+    # Print current frame
+    print(feature_dict[0][0])
 
-        # Sending messages to the server
-        my_message = json.dumps(feature_dict[0][0])
-        my_message = "interpreter;data;" + my_message + "$"
-        # Encode the string to utf-8 and write it to the pipe defined above
-        os.write(pipe_out, my_message.encode("utf-8"))
-        sys.stdout.flush()
+    # Sending messages to the server
+    my_message = json.dumps(feature_dict[0][0])
+    my_message = "interpreter;data;" + my_message + "$"
+    # Encode the string to utf-8 and write it to the pipe defined above
+    os.write(pipe_out, my_message.encode("utf-8"))
+    sys.stdout.flush()
 
-        # Send wrong signal
-        playsound('wrong.mp3')
+    # Send wrong signal
+    playsound('wrong.mp3')
 
-    # Define first photo
-    vlabel = tk.Label(master, image = master.photos[0])
-    vlabel.pack()
+# Define first photo
+vlabel = tk.Label(master, image = master.photos[0])
+vlabel.pack()
 
-    # Set index
-    global_index = 0
+# Set index
+global_index = 0
 
-    b1 = tk.Button(master, text="Next", command=lambda: nextCallback(global_index, end))
-    b2 = tk.Button(master, text="Wrong", command=lambda: wrongCallback(global_index))
-    #b1.pack()
-    #b2.pack()
+b1 = tk.Button(master, text="Next", command=lambda: nextCallback(global_index, end))
+b2 = tk.Button(master, text="Wrong", command=lambda: wrongCallback(global_index))
+#b1.pack()
+#b2.pack()
 
-    # Get key read from the presenter
-    def key(event):
-        #Up Left: u'\uf72c'
-        #Up Right: u'\uf72d'
-        #Down Left: u'\uf708'
-        #Down Right: '.'
+# Get key read from the presenter
+def key(event):
+    #Up Left: u'\uf72c'
+    #Up Right: u'\uf72d'
+    #Down Left: u'\uf708'
+    #Down Right: '.'
 
-        # Up Left: Wrong item
-        if repr(event.char) == "u'\uf72c'":
-            wrongCallback(global_index)
+    # Up Left: Wrong item
+    if repr(event.char) == "u'\uf72c'":
+        wrongCallback(global_index)
 
-        # Up Right: Next step (Right item)
-        if repr(event.char) == "u'\uf72d'":
-            nextCallback(global_index, end)
+    # Up Right: Next step (Right item)
+    if repr(event.char) == "u'\uf72d'":
+        nextCallback(global_index, end)
 
-    # Frame for detecting key events
-    frame = Frame(master, highlightbackground="green", highlightcolor="green", highlightthickness=1, width=100, height=100, bd=0)
-    frame.bind("<Key>", key)
-    frame.focus_force()
-    frame.pack(side="left", fill="both", expand=True)
+# Frame for detecting key events
+frame = Frame(master, highlightbackground="green", highlightcolor="green", highlightthickness=1, width=100, height=100, bd=0)
+frame.bind("<Key>", key)
+frame.focus_force()
+frame.pack(side="left", fill="both", expand=True)
 
-    tk.mainloop()
+tk.mainloop()
 
-    # Close the client safely, not always necessary
-    client.close() # Tell it to close
-    client.join() # Wait for it to close
+# Close the client safely, not always necessary
+client.close() # Tell it to close
+client.join() # Wait for it to close
