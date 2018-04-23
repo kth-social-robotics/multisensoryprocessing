3975747187b16feac1721f4506d26da347823eb8
KTH PMIL <czn423@alumni.ku.dk>
2018-04-23 09:34:26 +0200

    Update IP.

--- python/sensors/tobii/tobii_sensor.py
+++ python/sensors/tobii/tobii_sensor.py
@@ -1,6 +1,6 @@
 # py -2 tobii_sensor.py white 130.237.67.212 (glasses1)
-# py -2 tobii_sensor.py blue 130.237.67.191 (glasses2)
-# py -2 tobii_sensor.py brown 130.237.67.86 (glasses3)
+# py -2 tobii_sensor.py blue 130.237.67.171 (glasses2)
+# py -2 tobii_sensor.py brown 130.237.67.83 (glasses3)
 
 import pika
 import sys
